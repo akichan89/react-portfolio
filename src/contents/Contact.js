06ad6850d8ffc1a479348c6ee48ee8a4521433f1
akichan89 <62314744+akichan89@users.noreply.github.com>
2021-01-10 16:07:40 -0700

    Add files via upload

---
+++ src/contents/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email  :   autumn.e.gehring@gmail.com</h3>
+            <h3>Instagram   :   @ladyindev</h3>
+            <h3>Phone   :   385.343.8883</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
+    
